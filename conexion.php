307a3ea8455164339ca8c747dc3a37c448546c80
frankpezo <frankanderpg.2000@gmail.com>
2023-03-03 22:17:22 -0500

    Conexión a la base de datos de SQL SERVER

--- conexion.php
+++ conexion.php
@@ -1,13 +1,25 @@
 <?php
 //1. Creamos las varibles
+/* 
+//a. Variables usando el localhost
 $user = 'root';
 $pass = '';
 $host = 'localhost';
-$db = 'practiceapi';
+$db = 'practiceapi'; */
+
+//b. Para usarlo con el SQL server
+$user ='';
+$pass ='';
+$host ='LAPTOP-A7ED93UJ';
+$db ='practiceapi';
 
 //2. Intentamos conectarnos
 try{
-    $conexion = new PDO("mysql:host=$host;dbname=$db", $user,$pass);
+    /*  //a. Para usar con el localhost
+    $conexion = new PDO("mysql:host=$host;dbname=$db", $user,$pass); */
+    
+    //b. Para usarlo con SQL Server
+    $conexion = new PDO("sqlsrv:server=$host; database= $db", $user, $pass);
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //echo 'Conectado';
 
